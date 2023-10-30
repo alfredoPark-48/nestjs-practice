a47dd05f0083c500a9381ec566f3ab2c730ed63c
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2023-10-29 23:51:07 -0700

    Added functions to user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,12 +5,40 @@ import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private repo: Repository<User>) {
-        this.repo = repo;
-    }
+    constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
     create(email: string, password: string) {
         const user = this.repo.create({ email, password });
-        return this.repo.save(user);
+        return this.repo.save(user); // Must pass an entity for hooks to be called
+    }
+
+    findOne(id: number) {
+        return this.repo.findOne({ where: { id: id } });
+    }
+
+    find(email: string) {
+        return this.repo.findOne({ where: { email: email } });
+    }
+
+    // A Partial is a type helper defined in typescript (no import)
+    // This basically tells us that attrs can be any object that
+    // has at least or none some of the properties of the user class.
+    // Ex. an object with no properties, has at least one property or
+    // has all the properties
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.repo.findOne({ where: { id: id } });
+        if (!user) {
+            throw new Error('user not found');
+        }
+        Object.assign(user, attrs);
+        return this.repo.save(user); // Must pass an entity for hooks to be called
+    }
+
+    async remove(id: number) {
+        const user = await this.repo.findOne({ where: { id: id } });
+        if (!user) {
+            throw new Error('user not found');
+        }
+        return this.repo.remove(user); // Must pass an entity for hooks to be called
     }
 }
